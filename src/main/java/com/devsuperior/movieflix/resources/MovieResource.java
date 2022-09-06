d84326f073cc0a05fc62d9a191207a25f241e3bc
Adriano Abreu <adriano.luis.machado2013@hotmail.com>
2022-09-06 20:45:22 -0300

    entrega do endpoint Movie by id

--- src/main/java/com/devsuperior/movieflix/resources/MovieResource.java
+++ src/main/java/com/devsuperior/movieflix/resources/MovieResource.java
@@ -7,7 +7,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.devsuperior.movieflix.dto.MovieDTO;
 import com.devsuperior.movieflix.dto.UserDTO;
+import com.devsuperior.movieflix.services.MovieService;
 import com.devsuperior.movieflix.services.UserService;
 
 @RestController
@@ -15,17 +17,13 @@ import com.devsuperior.movieflix.services.UserService;
 public class MovieResource {
 
 	@Autowired
-	private UserService service;
+	private MovieService service;
 	
-//	@GetMapping(value = "/{id}")
-//	public ResponseEntity<UserDTO> findById(@PathVariable Long id){	
-//		UserDTO dto = service.findById(id);
-//		return ResponseEntity.ok().body(dto);
-//	}
-	
-	@GetMapping(value = "/profile")
-	public ResponseEntity<UserDTO> currentUserProfile(){		
-		UserDTO dto = service.currentUserProfile();
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<MovieDTO> findById(@PathVariable Long id){	
+		MovieDTO dto = service.findById(id);
 		return ResponseEntity.ok().body(dto);
 	}
+	
+
 }
--- src/main/java/com/devsuperior/movieflix/services/MovieService.java
+++ src/main/java/com/devsuperior/movieflix/services/MovieService.java
@@ -11,8 +11,11 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.movieflix.dto.MovieDTO;
 import com.devsuperior.movieflix.dto.UserDTO;
+import com.devsuperior.movieflix.entities.Movie;
 import com.devsuperior.movieflix.entities.User;
+import com.devsuperior.movieflix.repositories.MovieRepository;
 import com.devsuperior.movieflix.repositories.UserRepository;
 import com.devsuperior.movieflix.services.AuthService;
 import com.devsuperior.movieflix.services.MovieService;
@@ -24,10 +27,17 @@ public class MovieService {
 	private static Logger logger = LoggerFactory.getLogger(MovieService.class);
 	
 	@Autowired
-	private UserRepository repository;
+	private MovieRepository repository;
 	
 	@Autowired
 	private AuthService authService;
-	
+
+	@Transactional(readOnly = true)
+	public MovieDTO findById(Long id) {
+		Optional<Movie> obj = repository.findById(id);
+//		Product entity = obj.get();
+		Movie entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+		return new MovieDTO(entity);
+	}
 
 }
